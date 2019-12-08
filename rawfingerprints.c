8437ac5c43d587fe9190e3c7299935a3fda5a10b
gurushida <49831684+gurushida@users.noreply.github.com>
2019-12-08 15:23:10 +0100

    Made raw fingerprint generation multithreaded

--- rawfingerprints.c
+++ rawfingerprints.c
@@ -1,7 +1,19 @@
 #include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "rawfingerprints.h"
+
+
+#define N_THREADS 8
+
+
+struct build_rawfingerprints_job {
+    struct spectral_image* images;
+    struct rawfingerprint* fingerprints;
+    unsigned int first_image;
+    unsigned int last_image;
+};
 
 
 /**
@@ -46,6 +58,30 @@ static void convert_top_wavelets(struct coeff_and_index* sorted_data, struct raw
 }
 
 
+static void* launch_build_rawfingerprints(struct build_rawfingerprints_job* job) {
+     unsigned int N = NUMBER_OF_BINS * SPECTRAL_IMAGE_WIDTH;
+    struct coeff_and_index temp[N];
+
+    for (unsigned int i = job->first_image ; i <= job->last_image ; i++) {
+        // For each spectral image, let's copy the coefficients
+        // and their positions into the temp array
+        for (unsigned int j = 0 ; j < N ; j++) {
+            temp[j].coeff = job->images[i].image[j];
+            temp[j].index = j;
+        }
+
+        // Let's sort this array
+        qsort(temp, N, sizeof(struct coeff_and_index), (int (*)(const void *, const void *)) compare_by_absolute_values);
+
+        // Let's retain the 200 highest wavelet coefficients and convert them
+        // to 01, 10 or 00 whether they are negative, positive or null
+        convert_top_wavelets(temp, &(job->fingerprints[i]));
+    }
+
+    return NULL;
+}
+
+
 struct rawfingerprints* build_raw_fingerprints(struct spectral_images* haar_transformed_images) {
     unsigned int n_images = haar_transformed_images->n_images;
 
@@ -61,23 +97,24 @@ struct rawfingerprints* build_raw_fingerprints(struct spectral_images* haar_tran
         return NULL;
     }
 
-    unsigned int N = NUMBER_OF_BINS * SPECTRAL_IMAGE_WIDTH;
-    struct coeff_and_index temp[N];
+    pthread_t thread[N_THREADS];
+    struct build_rawfingerprints_job jobs[N_THREADS];
+    unsigned int images_per_thread = haar_transformed_images->n_images / N_THREADS;
+    for (unsigned int k = 0 ; k < N_THREADS ; k++) {
+        unsigned int start = k * images_per_thread;
+        unsigned int end = (k == N_THREADS - 1)
+                        ? haar_transformed_images->n_images - 1
+                        : (k + 1) * images_per_thread - 1;
+        jobs[k].images = haar_transformed_images->images;
+        jobs[k].fingerprints = rfp->fingerprints;
+        jobs[k].first_image = start;
+        jobs[k].last_image = end;
 
-    for (unsigned int i = 0 ; i < n_images ; i++) {
-        // For each spectral image, let's copy the coefficients
-        // and their positions into the temp array
-        for (unsigned int j = 0 ; j < N ; j++) {
-            temp[j].coeff = haar_transformed_images->images[i].image[j];
-            temp[j].index = j;
-        }
+        pthread_create(&(thread[k]), NULL, (void* (*)(void*))launch_build_rawfingerprints, &(jobs[k]));
+    }
 
-        // Let's sort this array
-        qsort(temp, N, sizeof(struct coeff_and_index), (int (*)(const void *, const void *)) compare_by_absolute_values);
-
-        // Let's retain the 200 highest wavelet coefficients and convert them
-        // to 01, 10 or 00 whether they are negative, positive or null
-        convert_top_wavelets(temp, &(rfp->fingerprints[i]));
+    for (unsigned int k = 0 ; k < N_THREADS ; k++) {
+	    pthread_join(thread[k], NULL);
     }
 
     return rfp;
